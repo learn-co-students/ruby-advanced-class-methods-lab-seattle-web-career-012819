3de2cae10437a142515ae8d7d63301f622826a7c
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 22:45:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,64 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |element|
+      element.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|obj| obj.name}
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(".")[0].split(" - ")
+    song = Song.new
+    song.name = info[1]
+    song.artist_name = info[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    info = filename.split(".")[0].split(" - ")
+    song = Song.new
+    song.name = info[1]
+    song.artist_name = info[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all = @@all.clear
   end
 
 end
