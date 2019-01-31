6af8143ed7b50b49df7b9f00c2cd5b42b0d5c179
github username <github email address>
2019-01-31 03:39:44 -0800

    First commit.

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,57 @@ class Song
 
   def self.all
     @@all
-  end
+  end   
 
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
 
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    song = @@all.select { |ref| ref.name==name }
+    song = (song==nil) ? false : song[0]
+  end
+
+  def self.find_or_create_by_name(name)
+    found_song = self.find_by_name(name)
+    made_song = !found_song ? self.create_by_name(name) : false
+    !found_song ? made_song : found_song
+  end
+
+  def self.alphabetical
+    @@all.sort { |a,b| a.name <=> b.name }
+  end
+
+  def self.new_from_filename(file_name)
+    song = self.create
+    sep = file_name =~ / - /
+    song.name = file_name.chars[sep + 3..file_name.length-5].join("")
+    song.artist_name = file_name.chars[0..sep-1].join("")
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.new_from_filename(file_name)
+    song
+  end
+
+  def self.destroy_all
+    until @@all.empty? do @@all.pop end
+  end
 end
